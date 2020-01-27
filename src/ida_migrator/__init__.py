3fe8b429165a932f03d599fd77d6fe968f7c8276
Gilad Reich <giladreichgr@gmail.com>
2020-01-27 01:25:41 +0100

    Add global information file to store version and paths.

---
+++ src/ida_migrator/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+VERSION = '1.0.0'
+PLUGIN_DIR = os.path.dirname(os.path.realpath(__file__))
+IDA_DIR = os.path.abspath(os.path.join(PLUGIN_DIR, '..', '..'))
+UI_DIR = os.path.join(PLUGIN_DIR, 'ui')

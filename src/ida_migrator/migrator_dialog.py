1f1cf8688bf99aa3281be1932264fa702a249ea7
Gilad Reich <giladreichgr@gmail.com>
2020-01-27 02:10:27 +0100

    Implement ability to search through functions.

--- src/ida_migrator/migrator_dialog.py
+++ src/ida_migrator/migrator_dialog.py
@@ -7,7 +7,7 @@ import idautils
 from ida_migrator import UI_DIR
 
 from PyQt5 import uic
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 
 Ui_MigratorDialog, MigratorDialogBase = uic.loadUiType(
@@ -21,6 +21,9 @@ col_CheckBox = 0
 col_Address  = 1
 col_Name     = 2
 
+# Search debounce/delay time
+FILTER_DEBOUNCE_PERIOD = 500
+
 
 class MigratorDialog(MigratorDialogBase):
 
@@ -30,6 +33,9 @@ class MigratorDialog(MigratorDialogBase):
         self._ui = Ui_MigratorDialog()
         self._ui.setupUi(self)
 
+        self.filter_timer = QTimer()
+        self.filter_timer.setSingleShot(True)
+        self.filter_text = ""
         self.tblFunctions = self._ui.tblFunctions
 
         self.connect_slots()
@@ -38,6 +44,8 @@ class MigratorDialog(MigratorDialogBase):
 
     def connect_slots(self):
         self._ui.btnStart.clicked.connect(self.on_start_clicked)
+        self._ui.tbxSearch.textChanged.connect(self.on_search_textchanged)
+        self.filter_timer.timeout.connect(self.filter_items)
 
     def adjust_table_layout(self):
         checkbox_width = 22
@@ -67,3 +75,21 @@ class MigratorDialog(MigratorDialogBase):
     # virtual
     def on_start_clicked(self):
         print("[IDA Migrator]: BASE - on_start_clicked")
+
+    def on_search_textchanged(self, text):
+        self.filter_text = text
+        self.filter_timer.start(FILTER_DEBOUNCE_PERIOD)
+
+    # TODO(Gilad): Convert this to use Qt Model/View architecture (QAbstractItemView)
+    def filter_items(self):
+        filter = self.filter_text.lower()
+        rowCount = self.tblFunctions.rowCount()
+        colCount = self.tblFunctions.columnCount()
+        for row in range(rowCount):
+            match = False
+            for col in range(1, colCount):
+                item = self.tblFunctions.item(row, col)
+                if item.text().lower().find(filter) != -1:
+                    match = True
+                    break
+            self.tblFunctions.setRowHidden(row, not match)

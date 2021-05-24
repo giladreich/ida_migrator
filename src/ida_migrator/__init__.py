e6b361621d67f895a47a4977e810fff2a44d7dd2
Gilad Reich <giladreichgr@gmail.com>
2021-05-24 21:56:33 +0200

    Bump version to 2.0.0.

--- src/ida_migrator/__init__.py
+++ src/ida_migrator/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '1.0.0'
+VERSION = '2.0.0'
 PLUGIN_NAME = "IDA Migrator"
 PLUGIN_DIR = os.path.dirname(os.path.realpath(__file__))
 IDA_DIR = os.path.abspath(os.path.join(PLUGIN_DIR, '..', '..'))
